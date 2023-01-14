ef20436ba69ad988d7d32cce130697505605853e
ahmed mohsen <ahmed.mohsen.web@gmail.com>
2023-01-14 12:32:17 +0200

    appjs styling

--- server.js
+++ server.js
@@ -1,19 +1,71 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
+const dataArray = [];
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+// listen to server
+const port = 7000;
+app.listen(port || 4000, () => {
+  console.log("server is running");
+  console.log("on port " + port);
+});
+
+/* routes */
+// ## post
+app.post("/post", (req, res) => {
+  console.log(req.body);
+  projectData.temp = req.body.temp;
+  projectData.content = req.body.content;
+  projectData.date = req.body.date;
+  dataArray.push(projectData);
+  console.log(dataArray);
+
+  // projectData[0] = req.body;
+  console.log(projectData);
+});
+// ## get
+app.get("/get", (req, res) => {
+  console.log(projectData);
+  res.send(projectData);
+});
+
+// Setup empty JS object to act as endpoint for all routes
+// Express to run server and routes
+
+// Start up an instance of app
+
+/* Dependencies */
+/* Middleware*/
+
+//Here we are configuring express to use body-parser as middle-ware.
+// Cors for cross origin allowance
+
+// Initialize the main project folder
+
+// Spin up the server
+// Callback to debug
+
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+
+// Post Route
--- website/app.js
+++ website/app.js
@@ -1,5 +1,80 @@
 /* Global Variables */
+const apiKey = "&appid=d0908eba7de46c86b9f2703c3c46351e";
+const generate = document.getElementById("generate");
+const zip = document.getElementById("zip");
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const date = document.getElementById("date");
+const temp = document.getElementById("temp");
+const content = document.getElementById("content");
+//   "https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+generate.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("do");
+  console.log(zip.value);
+  getData(baseUrl + zip.value + apiKey).then((data) => {
+    console.log("then");
+    postData("http://localhost:7000/post", {
+      temp: data.main.temp,
+      content: newDate,
+      date: newDate,
+    });
+  });
+  //   then((data) => {
+  //     date.innerText = newDate;
+  //     content.innerText = data.temp;
+  //     temp.innerText = data.main.temp;
+  //   });
+});
+
+const getData = async (url) => {
+  const response = await fetch(url);
+  try {
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const postData = async (
+  url = "http://localhost:7000/post",
+  data = {
+    temp: "test",
+    date: "web test",
+    content: "test",
+  }
+) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+// Personal API Key for OpenWeatherMap API
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+/* Function to GET Project Data */
